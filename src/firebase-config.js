74934737b7199b8363dcc9643ecd1c24224910ad
jameschiar <jchiar@ucdavis.edu>
2022-08-23 20:23:49 -0700

    added dotenv file
    secure those firebase keys

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -8,14 +8,22 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
+const API_KEY = import.meta.env.VITE_API_KEY
+const AUTH_DOMAIN = import.meta.env.VITE_AUTH_DOMAIN
+const PROJECT_ID = import.meta.env.VITE_PROJECT_ID
+const STORAGE_BUCKET= import.meta.env.VITE_STORAGE_BUCKET
+const MESSAGING_SENDER_ID = import.meta.env.VITE_MESSAGING_SENDER_ID
+const APP_ID= import.meta.env.VITE_APP_ID
+const MEASUREMENT_ID= import.meta.env.VITE_MEASUREMENT_ID
+
 const firebaseConfig = {
-  apiKey: 'AIzaSyBVe5EXHTpRPIiNSAGEkstZtpIWxmz2Mfw',
-  authDomain: "aggie-house.firebaseapp.com",
-  projectId: "aggie-house",
-  storageBucket: "aggie-house.appspot.com",
-  messagingSenderId: "509740822970",
-  appId: "1:509740822970:web:22da7e2fc3e1b3b9c76d83",
-  measurementId: "G-K0TCQC23WT"
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID,
+  measurementId: MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
