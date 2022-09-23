d82327af74b2f8d7cd814c467683647e330501e4
jameschiar <jchiar@ucdavis.edu>
2022-09-22 23:27:20 -0700

    Update firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -10,21 +10,15 @@ import { getStorage } from "firebase/storage";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const API_KEY = import.meta.env.VITE_API_KEY;
-const AUTH_DOMAIN = import.meta.env.VITE_AUTH_DOMAIN;
-const PROJECT_ID = import.meta.env.VITE_PROJECT_ID;
-const STORAGE_BUCKET = import.meta.env.VITE_STORAGE_BUCKET;
-const MESSAGING_SENDER_ID = import.meta.env.VITE_MESSAGING_SENDER_ID;
-const APP_ID = import.meta.env.VITE_APP_ID;
-const MEASUREMENT_ID = import.meta.env.VITE_MEASUREMENT_ID;
 
 const firebaseConfig = {
   apiKey: API_KEY,
-  authDomain: AUTH_DOMAIN,
-  projectId: PROJECT_ID,
-  storageBucket: STORAGE_BUCKET,
-  messagingSenderId: MESSAGING_SENDER_ID,
-  appId: APP_ID,
-  measurementId: MEASUREMENT_ID,
+  authDomain: "aggie-house.firebaseapp.com",
+  projectId: "aggie-house",
+  storageBucket: "aggie-house.appspot.com",
+  messagingSenderId: "509740822970",
+  appId: "1:509740822970:web:22da7e2fc3e1b3b9c76d83",
+  measurementId: "G-K0TCQC23WT",
 };
 
 const app = initializeApp(firebaseConfig);
