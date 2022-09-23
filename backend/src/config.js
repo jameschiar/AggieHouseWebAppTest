397dc13e7fa7925f00f7e81e1776281170d3bac2
jameschiar <jchiar@ucdavis.edu>
2022-09-23 00:07:59 -0700

    Update config.js

--- backend/src/config.js
+++ backend/src/config.js
@@ -22,13 +22,14 @@ cfg.firebaseConfig = {
   type: process.env['type'],
   project_id: process.env['project_id'],
   private_key_id: process.env['private_key_id'],
-  private_key: process.env['private_key'],
+  private_key: process.env['private_key'].replace(/\\n/g, '\n'),
   client_email: process.env['client_email'],
   client_id: process.env['client_id'],
-  auth_uri: process.env['auth_uri'],
-  token_uri: process.env['token_uri'],
-  auth_provider_x509_cert_url: process.env['auth_provider_x509_cert_url'],
-  client_x509_cert_url: process.env['client_x509_cert_url'],
+  auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+  token_uri: 'https://oauth2.googleapis.com/token',
+  auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+  client_x509_cert_url:
+    'https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-8j0lv%40aggie-house.iam.gserviceaccount.com',
 };
 
 // Export configuration object
