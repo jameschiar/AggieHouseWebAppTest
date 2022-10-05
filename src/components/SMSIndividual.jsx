6e143c5492e06e89c3390262049b8ede8eebfbc1
jameschiar <jchiar@ucdavis.edu>
2022-10-05 14:25:36 -0700

    choose a volunteer by name
    instead of typing in number of volunteer, can now choose volunteer's name and send message

--- src/components/SMSIndividual.jsx
+++ src/components/SMSIndividual.jsx
@@ -1,48 +1,60 @@
-import React, {useState} from 'react';
-import styled from 'styled-components';
+import React, { useState } from "react";
+import styled from "styled-components";
+import Select from "react-select";
 import { useUser } from "../context/UserProvider";
 
 function SMSIndividual() {
-  const [sms, setSms] = useState('');
-  const [number, setNumber] = useState('');
+  const [sms, setSms] = useState("");
+  const [number, setNumber] = useState("");
   const { users } = useUser();
 
+  const handleSelectChange = (data) => {
+    setNumber(data.value.phoneNumber);
+  };
 
   const sendSms = (event) => {
     event.preventDefault();
 
     let smsObj = {
-        mobile_number: number,
-        message: sms,
-    }
+      mobile_number: number,
+      message: sms,
+    };
 
-    fetch(`https://aggie-house-reminders.herokuapp.com/send-text?recipient=${smsObj.mobile_number}&textmessage=${smsObj.message}`)
-    .catch(err => console.error(err))
-    
-    alert("Text Sent")
-  }
-  
-    
-              
+    fetch(
+      `https://aggie-house-reminders.herokuapp.com/send-text?recipient=${smsObj.mobile_number}&textmessage=${smsObj.message}`
+    ).catch((err) => console.error(err));
 
+    alert("Text Sent");
+  };
 
   const handleChange = (event) => {
-    if (event.target.name === 'number') {
+    if (event.target.name === "number") {
       setNumber(event.target.value);
-    } else if (event.target.name === 'sms') {
+    } else if (event.target.name === "sms") {
       setSms(event.target.value);
     }
-  
-  }
+  };
 
   return (
     <Container>
       <Header>Send SMS Message</Header>
       <Form onSubmit={sendSms}>
         <label>Volunteer:</label>
-        <Input name='number' onChange={handleChange}></Input>
+        <Select
+          styles={customStyles}
+          width="200px"
+          maxMenuHeight={150}
+          options={users.map((user) => {
+            return {
+              value: user,
+              label: user.displayName,
+            };
+          })}
+          placeholder="Assign"
+          onChange={handleSelectChange}
+        />
         <label>Message:</label>
-        <TextArea name='sms' onChange={handleChange}></TextArea>
+        <TextArea name="sms" onChange={handleChange}></TextArea>
         <Button className="mini-button">Submit</Button>
       </Form>
     </Container>
@@ -51,31 +63,54 @@ function SMSIndividual() {
 
 export default SMSIndividual;
 
+// custom styling for react-select
+const customStyles = {
+  menu: (provided, state) => ({
+    ...provided,
+    width: state.selectProps.width,
+    borderBottom: "1px dotted pink",
+    color: state.selectProps.menuColor,
+    padding: 20,
+  }),
+  control: (provided, { selectProps: { width, height, fontSize } }) => ({
+    ...provided,
+    width: width,
+    height: height,
+  }),
+
+  singleValue: (provided, state) => {
+    const opacity = state.isDisabled ? 0.5 : 1;
+    const transition = "opacity 300ms";
+
+    return { ...provided, opacity, transition };
+  },
+};
+
 const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: left;
   justify-content: center;
-  
-  background-color: #E8E2DC;
-`
+
+  background-color: #e8e2dc;
+`;
 
 const Header = styled.h3`
   text-transform: uppercase;
   letter-spacing: 1px;
-`
+`;
 
 const Form = styled.form`
   display: flex;
   flex-direction: column;
   align-items: left;
-`
+`;
 
 const Input = styled.input`
   width: 50%;
   padding: 12px;
   margin: 6px 0 4px;
-`
+`;
 
 const TextArea = styled.textarea`
   width: 50%;
@@ -83,11 +118,10 @@ const TextArea = styled.textarea`
   padding: 12px;
   margin: 6px 0 4px;
   height: 100px;
-`
+`;
 
 const Button = styled.button`
-  
   &:hover {
     cursor: pointer;
   }
-`
+`;
